8ed74eb3a77d9787ddf8a804428c5456f8afa59c
ManishA0799 <manishatyalkar29@gmail.com>
2022-09-29 16:41:39 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Class_HW_28_9
+{
+     
+        public class Customer
+        {
+            public int CustomerId { get; set; }
+            public string CustomerLoan { get; set; }
+        }
+
+        public class Account
+        {
+            public int Aid { get; set; }
+            public string Aname { get; set; }
+
+            public List<Customer> Customers = new List<Customer>();
+        }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Class_HW_28_9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accountList = new List<Account>()
+            {
+                new Account
+                {
+                    Aid=1,Aname="Saving",
+                    Customers=
+                    {
+                    new Customer{CustomerId=1,CustomerLoan="Home Loan"},
+                    new Customer{CustomerId=1,CustomerLoan="Car Loan"},
+                    new Customer{CustomerId=2,CustomerLoan="Home Loan"},
+                    }
+                },
+                new Account
+                {
+                      Aid=1,Aname="Current",
+                    Customers=
+                    {
+                    new Customer{CustomerId=10,CustomerLoan="Gold Loan" },
+                   
+                    }
+                },
+            };
+
+            foreach (Account a in accountList)
+            {
+                Console.WriteLine(a.Aname);
+                foreach (Customer c in a.Customers )
+                {
+                    Console.WriteLine("\t"+c.CustomerId+ "\t"+ c.CustomerLoan);
+                }
+            }
+
+
+
+
+            
+        }
+    }
+}
